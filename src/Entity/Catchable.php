3763c0c3497d6ee9a5c16123264e3ffb136e2643
Martin Kirilov <wucdbm@gmail.com>
2018-03-30 00:00:27 +0300

    remove index on message, file

--- src/Entity/Catchable.php
+++ src/Entity/Catchable.php
@@ -19,9 +19,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="Forci\Bundle\Catchable\Repository\CatchableRepository")
  * @ORM\Table(name="forci_catchable__catchables", options={"collate"="utf8mb4_unicode_ci", "charset"="utf8mb4"}, indexes={
- *     @ORM\Index(name="class", columns={"class"}),
- *     @ORM\Index(name="message", columns={"message"}),
- *     @ORM\Index(name="file", columns={"file"})
+ *     @ORM\Index(name="class", columns={"class"})
  * })
  */
 class Catchable {
